d485b76ea89e3117e0f4e41a253745b649eb812d
杨卓 <1007510178@QQ.com>
2019-12-12 10:41:00 +0800

    exe

---
+++ src/router/routerConfig.js
@@ -0,0 +1,21 @@
+
+
+
+const routes=[
+    {
+        path:"/index",
+        component:Index
+    },
+    {
+        path:"/detail",
+        component:Detail
+    },
+    {
+        path:"/login",
+        component:Login
+    },
+    {
+        path:"/",
+        to:"/index"
+    }
+]
